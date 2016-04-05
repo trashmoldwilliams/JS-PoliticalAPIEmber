dfb643fddc94b8d10b525f348fe9040490170c75
Tim Durward and Will Johnson <Guest@epicodus-45.local>
2016-04-05 16:39:19 -0700

    Add output of committee values.

--- app/routes/committee-results.js
+++ app/routes/committee-results.js
@@ -2,17 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    console.log("test");
-    var output = ["stringTest"];
-    for(var i = 1; i <= 5; i++) {
-     var url = 'http://congress.api.sunlightfoundation.com/committees?chamber=senate&page=' + i + '&apikey=858d9aa76d2144b196ed0ce17edd5954';
-      Ember.$.getJSON(url).then(function(responseJSON) {
-      for(var j = 0; j < responseJSON.results.length; j++) {
-         output.push(responseJSON.results[j].name);
-       }
-     });
-   }
-   console.log(output);
-   return output;
+    var output = [];
+    for(var i = 1; i <= 2; i++) {
+      var url = 'http://congress.api.sunlightfoundation.com/committees?chamber=senate&per_page=50&page=' + i + '&apikey=858d9aa76d2144b196ed0ce17edd5954';
+       Ember.$.getJSON(url).then(function(responseJSON) {
+        for(var j = 0; j < responseJSON.results.length; j++) {
+          output.push(responseJSON.results[j].name);
+        }
+        setTimeout(function(){ console.log( output)},1100);
+
+      });
+    }
   }
 });
