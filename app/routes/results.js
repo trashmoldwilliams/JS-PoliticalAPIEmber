74c2fd19751e7406dbe833551ec08ba74f19bd23
Tim Durward and Will Johnson <timothy.a.durward@gmail.com>
2016-04-05 08:28:14 -0700

    setup initial api stack

---
+++ app/routes/results.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function(params) {
+   var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=858d9aa76d2144b196ed0ce17edd5954&zip=' + params.zip;
+   return Ember.$.getJSON(url).then(function(responseJSON) {
+     return responseJSON.results;
+   });
+  }
+});
